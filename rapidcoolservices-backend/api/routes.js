4d70521ff25f757240752b6f0636f3219b6655fb
khanfarhan2455-code <khanfarhan2455@gmail.com>
2026-07-08 03:32:51 +0530

    Delete rapidcoolservices-backend/api/routes.js

--- rapidcoolservices-backend/api/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/test', (req, res) => {
-  res.json({ status: "Success", message: "API is working!" });
-});
-
-export default router;
